48a189d87698f5de8affb10e1adf7df039f461ed
Greg Allen <Allen.Greg@bcg.com>
2015-05-02 00:38:40 -0400

    testing config

--- gruntfile.js
+++ gruntfile.js
@@ -6,8 +6,7 @@ module.exports = function(grunt) {
           'public/stylesheets/style.css' : 'sass/style.scss'
         }
       }
-    }
-
+    },
     watch: {
       source: {
         files: ['sass/**/*.scss'],
@@ -16,8 +15,26 @@ module.exports = function(grunt) {
           livereload: true,
         }
       }
-    }
+    },
+    coveralls: {
+      // Options relevant to all targets
+      options: {
+        // When true, grunt-coveralls will only print a warning rather than
+        // an error, to prevent CI builds from failing unnecessarily (e.g. if
+        // coveralls.io is down). Optional, defaults to false.
+        force: false
+      },
+
+      your_target: {
+        // LCOV coverage file (can be string, glob or array)
+        src: 'coverage-results/extra-results-*.info',
+        options: {
+          // Any options for just this target
+        }
+      },
+    },
 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-coveralls');
   grunt.registerTask('default', ['sass']);
 };
