70da01b6477e5a783b5ef364691486d82923c928
Greg Allen <Allen.Greg@bcg.com>
2015-05-01 22:55:48 -0400

    adding node modules

---
+++ gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+  grunt.initConfig ({
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css' : 'sass/style.scss'
+        }
+      }
+    }
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.registerTask('default', ['sass']);
+};
